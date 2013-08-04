568a7ef4dabc6bb70bb1d68c7fa4724c96c2f54a
Bruce Canedy <canedy@msn.com>
2013-08-04 13:55:35 -0400

    add status validations and tests

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
 
-  before_filter :authenticate_user!, only: [:new]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
 
   # GET /statuses
   # GET /statuses.json
--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -24,7 +24,30 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do 
+    post :create, status: { content: "Hello" }
+
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be logged in to edit a status" do 
+    get :edit, id: @status
+
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be logged in to update a status" do 
+    patch :update, id: @status, status: { content: "Hello" }
+
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:bruce)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -38,11 +61,15 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:bruce)
+    
     get :edit, id: @status
     assert_response :success
   end
 
   test "should update status" do
+    sign_in users(:bruce)
+    
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
