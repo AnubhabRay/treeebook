a11fe17f1176a90602a00d2479066928c43b75ad
Bruce Canedy <canedy@msn.com>
2013-08-03 23:47:22 -0400

    added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Treebook::Application.routes.draw do
   
   devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout    
+  end
+
   root to: 'statuses#index'
-  
+  get 'feed', to: 'statuses#index', as: :feed
   resources :statuses
 
 
