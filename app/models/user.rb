c39b4aadc4c86df83a3227bf24b427d1a4a275aa
Bruce Canedy <canedy@msn.com>
2013-08-03 22:54:21 -0400

    Added test for user first_name, last_name and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+ 	validates :first_name, :last_name, presence: true
+
+ 	validates :profile_name, presence: true, uniqueness: true, format: { with: /a-zA-Z0-9_-/, message: 'Must be formatted correclty.' }
+
   def full_name
   	first_name + " " + last_name
   end
